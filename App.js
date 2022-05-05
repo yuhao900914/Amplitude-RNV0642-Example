3a4cc6a03b2882f21e50196dcd3eff9659075609
yuhao900914 <yuhao900914@gmail.com>
2022-05-05 14:42:34 -0700

    init

---
+++ App.js
@@ -0,0 +1,140 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import { Amplitude } from '@amplitude/react-native';
+ import React from 'react';
+ import type {Node} from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+   Button
+ } from 'react-native';
+ 
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+ 
+ const Section = ({children, title}): Node => {
+   const isDarkMode = useColorScheme() === 'dark';
+   return (
+     <View style={styles.sectionContainer}>
+       <Text
+         style={[
+           styles.sectionTitle,
+           {
+             color: isDarkMode ? Colors.white : Colors.black,
+           },
+         ]}>
+         {title}
+       </Text>
+       <Text
+         style={[
+           styles.sectionDescription,
+           {
+             color: isDarkMode ? Colors.light : Colors.dark,
+           },
+         ]}>
+         {children}
+       </Text>
+     </View>
+   );
+ };
+ 
+ const App: () => Node = () => {
+   const isDarkMode = useColorScheme() === 'dark';
+  
+   const amplitude = Amplitude.getInstance();
+   amplitude.trackingSessionEvents(true);
+   amplitude.setUserId('TEST USER');
+   amplitude.init('YOUR API KEY');
+   const backgroundStyle = {
+     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+   };
+ 
+   return (
+     <SafeAreaView style={backgroundStyle}>
+       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+       <Button title={'Amplitude button'} onPress={()=>{
+         amplitude.logEvent(' on button click');
+         console.log(' on ampltidue button click');
+       }} >Amplitude click</Button>
+ 
+       <Button title={'Amplitude logRevenue'} onPress={()=>{
+         const userProperties = {
+           price: 7900,
+           productId: 'membership',
+           quantity: 1,
+           receipt: JSON.stringify({'test':123}),
+           receiptSignature: 'signatureAndroid',
+           eventProperties: {
+             product: 'product name',
+           },
+         };
+       
+         amplitude.logRevenue(userProperties);
+         console.log(' on ampltidue logRevenue click');
+       }} >Amplitude click</Button>
+ 
+       <ScrollView
+         contentInsetAdjustmentBehavior="automatic"
+         style={backgroundStyle}>
+         <Header />
+         <View
+           style={{
+             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+           }}>
+           <Section title="Step One">
+             Edit <Text style={styles.highlight}>App.js</Text> to change this
+             screen and then come back to see your edits.
+           </Section>
+           <Section title="See Your Changes">
+             <ReloadInstructions />
+           </Section>
+           <Section title="Debug">
+             <DebugInstructions />
+           </Section>
+           <Section title="Learn More">
+             Read the docs to discover what to do next:
+           </Section>
+           <LearnMoreLinks />
+         </View>
+       </ScrollView>
+     </SafeAreaView>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   sectionContainer: {
+     marginTop: 32,
+     paddingHorizontal: 24,
+   },
+   sectionTitle: {
+     fontSize: 24,
+     fontWeight: '600',
+   },
+   sectionDescription: {
+     marginTop: 8,
+     fontSize: 18,
+     fontWeight: '400',
+   },
+   highlight: {
+     fontWeight: '700',
+   },
+ });
+ 
+ export default App;
+ 
